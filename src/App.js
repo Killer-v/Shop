704e4a48c26094e0aad667c89fa0ecc26eaf6e3e
Violeta <88794494+Viola-lol@users.noreply.github.com>
2024-10-11 05:09:57 +0300

    add functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Items from "./components/items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул',
+          img: 'chair.jfif',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+          category: 'chairs',
+          price: '22,00',
+        },
+        {
+          id: 2,
+          title: 'Стул',
+          img: 'chair.jfif',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+          category: 'chairs',
+          price: '22,00',
+        },
+        {
+          id: 3,
+          title: 'Стул',
+          img: 'chair.jfif',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+          category: 'chairs',
+          price: '22,00',
+        },
+        {
+          id: 4,
+          title: 'Стул',
+          img: 'chair.jfif',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+          category: 'chairs',
+          price: '22,00',
+        },
+        {
+          id: 5,
+          title: 'Стул',
+          img: 'chair.jfif',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+          category: 'chairs',
+          price: '22,00',
+        },
+
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
