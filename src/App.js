9a765788d452ecfc6777943be4def5d36c152145
Violeta <88794494+Viola-lol@users.noreply.github.com>
2024-10-12 18:50:47 +0300

    added cart and categories

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Items from "./components/items";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -51,15 +54,46 @@ class App extends React.Component {
 
       ]
     }
+    this.state.currentItems = this.state.items
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
-        <Header />
-        <Items items={this.state.items}/>
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
+  }
+
+  chooseCategory(category) {
+    if (category == 'all') {
+      this.setState({ currentItems: this.state.items })
+      return
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter(el => el.id != id) })
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+
+    this.state.orders.forEach(el => {
+      if (el.id === item.id)
+        isInArray = true
+    })
+    if (!isInArray)
+      this.setState({ orders: [...this.state.orders, item] })
   }
 }
 
